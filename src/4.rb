5269b8706aa36ffa64fefffaa01fe7d9244bfcdd
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-12-09 19:49:53 +0900

    Problem 4

---
+++ src/4.rb
@@ -0,0 +1,12 @@
+require 'digest'
+
+key = $<.readline.chomp
+digest = Digest::MD5.new << key
+
+n = 0
+loop do
+  hex = digest.dup.update(n.to_s).hexdigest
+  break if hex.start_with?('0' * 5)
+  n += 1
+end
+puts n
