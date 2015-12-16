c01b340845024c810f9a96fde653aa273bf20e87
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-12-16 11:10:21 +0900

    Problem 13

---
+++ src/13b.rb
@@ -0,0 +1,25 @@
+def score(order, m)
+  0.upto(order.size - 1).map {|i|
+    j = (i + 1) % order.size
+    m[order[i]][order[j]] + m[order[j]][order[i]]
+  }.inject(:+)
+end
+
+m = Hash.new {|hash, key| hash[key] = Hash.new(0) }
+
+$<.each_line do |l|
+  if /^(?<a>\w+) would (?<pm>lose|gain) (?<point>\d+) happiness units by sitting next to (?<b>\w+)./ =~ l
+    m[a][b] = point.to_i * (pm == 'gain' ? 1 : -1)
+  end
+end
+
+names = ['Me'] + m.keys
+
+head = names[0]
+tail = names[1..-1]
+
+puts tail.permutation.collect {|perm|
+  perm.push(head)
+
+  score(perm, m)
+}.max
