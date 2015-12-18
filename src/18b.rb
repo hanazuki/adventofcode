0de733bd29860eb9ae4b2df647d21b73e62a9ed1
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-12-18 19:14:59 +0900

    Problem 18

---
+++ src/18b.rb
@@ -0,0 +1,37 @@
+state = $<.readlines.each(&:chomp!)
+
+HW = 100
+
+def l(state, i, j)
+  return false if i < 0 || j < 0 || i >= HW || j >= HW
+  state[i][j] == '#'
+end
+
+NEIBOURHOOD = [-1, 0, 1].product([-1, 0, 1]).tap{|a| a.delete([0, 0]) }
+
+def step(state)
+  new_state = Array.new(HW) { '#' * HW }
+
+  HW.times do |i|
+    HW.times do |j|
+      c = NEIBOURHOOD.count {|di, dj| l(state, i + di, j + dj) }
+      if l(state, i, j)
+        new_state[i][j] = c == 2 || c == 3 ? '#' : '.'
+      else
+        new_state[i][j] = c == 3 ? '#' : '.'
+      end
+    end
+  end
+
+  [0, HW-1].product([0, HW-1]).each do |i, j|
+    new_state[i][j] = '#'
+  end
+
+  new_state
+end
+
+100.times do
+  state = step(state)
+end
+
+puts state.map {|s| s.count('#') }.inject(:+)
